0a17bdd7f6ad0be713119ed4bc675baa1db22af4
Tejas Naik <rntejas2005@gmail.com>
2021-03-08 12:55:26 +0530

    added files

---
+++ strings.py
@@ -0,0 +1,10 @@
+my_string = "Hello to the World!"
+print(my_string)
+
+my_ml_string = """This is 
+multiline string"""
+print(my_ml_string)
+
+my_sl_string = """This is \
+multiline string"""
+print(my_sl_string)
